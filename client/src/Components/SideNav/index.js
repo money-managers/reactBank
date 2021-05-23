f9ba2926688879423706f4672336680070a12428
Kirk <kirkbalop@gmail.com>
2021-05-23 12:08:04 -0700

    fix deployment

--- client/src/Components/SideNav/index.js
+++ client/src/Components/SideNav/index.js
@@ -24,7 +24,7 @@ function SideNav(props){
             </a>
             {Auth.loggedIn() ? (
             <>
-            <a  href="#login"  onClick={Auth.logout} >
+            <a  href="#login"  onClick={Auth.logout}>
                 Log Out
             </a>
               </>
